ea383ed8c23552d46438f53020662000ffe91d53
minwoo129 <key0129mw@naver.com>
2023-08-05 16:07:45 +0900

    액션함수 추가

--- MoviePlayer/ViewController.swift
+++ MoviePlayer/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnPlayInternalMovie(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnPlayExternalMovie(_ sender: UIButton) {
+    }
 }
 
