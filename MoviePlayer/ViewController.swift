5d7196c78f0b9c776bda933d20a5bfab9ca91b57
minwoo129 <key0129mw@naver.com>
2023-08-05 16:10:59 +0900

    내부 동영상 파일 실행 코드 추가

--- MoviePlayer/ViewController.swift
+++ MoviePlayer/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,17 @@ class ViewController: UIViewController {
     }
 
     @IBAction func btnPlayInternalMovie(_ sender: UIButton) {
+        let filePath:String? = Bundle.main.path(forResource: "FastTyping", ofType: "mp4");
+        let url = NSURL(fileURLWithPath: filePath!);
+        
+        let playerController = AVPlayerViewController()
+        
+        let player = AVPlayer(url: url as URL);
+        playerController.player = player
+        
+        self.present(playerController, animated: true) {
+            player.play();
+        }
     }
     
     @IBAction func btnPlayExternalMovie(_ sender: UIButton) {
