df619e2510fed5e9ff1086d450de9f58d2709125
minwoo129 <key0129mw@naver.com>
2023-08-05 16:16:20 +0900

    동영상 실행 함수 추가

--- MoviePlayer/ViewController.swift
+++ MoviePlayer/ViewController.swift
@@ -19,17 +19,24 @@ class ViewController: UIViewController {
         let filePath:String? = Bundle.main.path(forResource: "FastTyping", ofType: "mp4");
         let url = NSURL(fileURLWithPath: filePath!);
         
-        let playerController = AVPlayerViewController()
-        
-        let player = AVPlayer(url: url as URL);
-        playerController.player = player
-        
-        self.present(playerController, animated: true) {
-            player.play();
-        }
+        playVideo(url: url)
     }
     
     @IBAction func btnPlayExternalMovie(_ sender: UIButton) {
+        let url = NSURL(string: "https://dl.dropboxusercontent.com/s/e38auz050w2mvud/Fireworks.mp4")!
+        
+        playVideo(url: url);
+    }
+    
+    private func playVideo(url:NSURL) {
+        let playerController = AVPlayerViewController()
+        
+        let player = AVPlayer(url: url as URL)
+        playerController.player = player;
+        
+        self.present(playerController, animated: true) {
+            player.play()
+        }
     }
 }
 
